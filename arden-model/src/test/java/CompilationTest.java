48623033d9abb6c253b3cb2851bdbeaa560788ab
Davide Sottara <dsotty@gmail.com>
2013-01-27 18:01:56 -0700

    Adding arden ontology and generated class model

---
+++ arden-model/src/test/java/CompilationTest.java
@@ -0,0 +1,40 @@
+import junit.framework.Assert;
+import org.drools.io.ResourceFactory;
+import org.drools.semantics.builder.DLFactoryBuilder;
+import org.drools.semantics.builder.model.OntoModel;
+import org.drools.shapes.OntoModelCompiler;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.util.List;
+
+import static junit.framework.Assert.assertFalse;
+
+public class CompilationTest {
+
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+
+    @Test
+    public void testArdenOntology() {
+
+        OntoModel arden = DLFactoryBuilder.newDLFactoryInstance().buildModel( "arden",
+                ResourceFactory.newClassPathResource( "ontologies/arden.owl" ),
+                OntoModel.Mode.OPTIMIZED );
+        Assert.assertNotNull( arden );
+
+        OntoModelCompiler compiler = new OntoModelCompiler( arden, folder.getRoot() );
+
+        List<Diagnostic<? extends JavaFileObject>> diags = compiler.compileOnTheFly( OntoModelCompiler.minimalOptions, OntoModelCompiler.MOJO_VARIANTS.JPA2 );
+        for ( Diagnostic dx : diags ) {
+            assertFalse( dx.getKind() == Diagnostic.Kind.ERROR );
+        }
+
+    }
+
+}
