9561951a8b05c5432690f2639533df50d7700668
Davide Sottara <dsotty@gmail.com>
2013-02-04 19:40:01 -0700

    Upgrade code to use latest ontology processing library, add uncommitted examples

--- editor-core/src/main/java/ModelManagerOwlAPIHermit.java
+++ editor-core/src/main/java/ModelManagerOwlAPIHermit.java
@@ -1,18 +1,24 @@
+import com.clarkparsia.empire.Empire;
+import com.clarkparsia.empire.EmpireOptions;
+import com.clarkparsia.empire.SupportsRdfId;
+import com.clarkparsia.empire.config.ConfigKeys;
+import com.clarkparsia.empire.config.EmpireConfiguration;
+import com.clarkparsia.empire.sesametwo.OpenRdfEmpireModule;
+import com.clarkparsia.empire.sesametwo.RepositoryDataSourceFactory;
+import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.sail.SailGraph;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
-import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory;
-import com.tinkerpop.blueprints.util.io.graphml.GraphMLWriter;
 import com.tinkerpop.blueprints.util.io.graphson.GraphSONWriter;
+import edu.asu.sharpc2b.prr.ExecutableRuleSet;
+import edu.asu.sharpc2b.prr.ExecutableRuleSetImpl;
+import edu.asu.sharpc2b.prr.ProductionRuleSetImpl;
 import org.coode.owlapi.rdf.model.AbstractTranslator;
 import org.coode.owlapi.rdf.model.RDFGraph;
-import org.coode.owlapi.rdf.model.RDFResourceNode;
 import org.coode.owlapi.rdf.model.RDFTranslator;
-import org.coode.owlapi.rdf.model.RDFTriple;
 import org.coode.owlapi.turtle.TurtleOntologyFormat;
-import org.openrdf.model.Graph;
+import org.drools.semantics.AbstractObjectGraphVisitor;
 import org.openrdf.model.Resource;
-import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.impl.LiteralImpl;
 import org.openrdf.model.impl.URIImpl;
@@ -22,7 +28,6 @@ import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.event.base.NotifyingRepositoryWrapper;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.sail.Sail;
-import org.openrdf.sail.helpers.SailWrapper;
 import org.openrdf.sail.memory.MemoryStore;
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -34,7 +39,6 @@ import org.semanticweb.owlapi.model.OWLIndividualAxiom;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyFormat;
 import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
@@ -46,29 +50,32 @@ import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 import org.semanticweb.owlapi.util.InferredAxiomGenerator;
 import org.semanticweb.owlapi.util.InferredClassAssertionAxiomGenerator;
-import org.semanticweb.owlapi.util.InferredDataPropertyCharacteristicAxiomGenerator;
 import org.semanticweb.owlapi.util.InferredEquivalentClassAxiomGenerator;
-import org.semanticweb.owlapi.util.InferredEquivalentDataPropertiesAxiomGenerator;
 import org.semanticweb.owlapi.util.InferredEquivalentObjectPropertyAxiomGenerator;
-import org.semanticweb.owlapi.util.InferredIndividualAxiomGenerator;
-import org.semanticweb.owlapi.util.InferredInverseObjectPropertiesAxiomGenerator;
-import org.semanticweb.owlapi.util.InferredObjectPropertyCharacteristicAxiomGenerator;
 import org.semanticweb.owlapi.util.InferredOntologyGenerator;
 import org.semanticweb.owlapi.util.InferredPropertyAssertionGenerator;
 import org.semanticweb.owlapi.util.InferredSubClassAxiomGenerator;
-import org.semanticweb.owlapi.util.InferredSubDataPropertyAxiomGenerator;
 import org.semanticweb.owlapi.util.InferredSubObjectPropertyAxiomGenerator;
+import org.w3._2002._07.owl.Thing;
+import org.w3._2002._07.owl.ThingImpl;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.spi.PersistenceProvider;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
 import java.io.PrintWriter;
-import java.util.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public class ModelManagerOwlAPIHermit {
 
@@ -173,23 +180,104 @@ public class ModelManagerOwlAPIHermit {
         convertToOwlAPI_RDF( onto );
 
 
-        dumpInSailRDFStore( onto );
+        Repository repo = dumpInSailRDFStore( onto );
 
 
-        com.tinkerpop.blueprints.Graph blueGraph = asPropertyGraph( onto );
+        Map<String, Object> aMap = new HashMap<String, Object>();
 
-//        File base = new File( ModelManagerOwlAPIHermit.class.getResource( "/ontologies/" + sources[0] ).getPath() );
-//        File dir = new File( base.getParent() + File.separator + "precompiled" );
-//        OutputStream outStream = new FileOutputStream( new File( dir.getAbsolutePath() + File.separator + "example1.gson" ) );
-        GraphSONWriter.outputGraph( blueGraph, System.err );
+        aMap.put(ConfigKeys.FACTORY, RepositoryDataSourceFactory.class.getName());
+        aMap.put(RepositoryDataSourceFactory.REPO_HANDLE, repo );
+
+        OpenRdfEmpireModule mod = new OpenRdfEmpireModule();
+               EmpireConfiguration ec = new EmpireConfiguration();
+               Empire.init( ec, mod );
+               EmpireOptions.STRICT_MODE = false;
+        EmpireOptions.ENFORCE_ENTITY_ANNOTATION = false;
+
+               PersistenceProvider aProvider = Empire.get().persistenceProvider();
+
+               EntityManagerFactory emf = aProvider.createEntityManagerFactory(
+                       "edu.asu.sharpc2b.example1:edu.asu.sharpc2b.lmm:edu.asu.sharpc2b.ocl:edu.asu.sharpc2b.ops:edu.asu.sharpc2b.prr:org.w3._2002._07.owl",
+                       aMap );
+               EntityManager em = emf.createEntityManager();
+
+        System.out.println( em.isOpen() );
+
+
+        long now = System.currentTimeMillis();
+        Object o = em.find( ProductionRuleSetImpl.class, URI.create( "http://asu.edu/sharpc2b/example1#ruleSet1" ) );
+        now =  System.currentTimeMillis() - now;
+        System.err.println( "Empire de-frosting " + now );
+
+        com.tinkerpop.blueprints.Graph blueGraph1 = asPropertyGraph( onto );
+
+        com.tinkerpop.blueprints.Graph blueGraph2 = asObjectGraph( (Thing) o );
+
+
+        em.close();
+        repo.shutDown();
+
+
+
+        File base = new File( ModelManagerOwlAPIHermit.class.getResource( "/ontologies/" + sources[0] ).getPath() );
+        File dir = new File( base.getParent()  );
+        OutputStream outStream = new FileOutputStream( new File( dir.getAbsolutePath() + File.separator + "example1.obj.gson" ) );
+        GraphSONWriter.outputGraph( blueGraph2, outStream );
 
     }
 
+    private com.tinkerpop.blueprints.Graph asObjectGraph( Thing o ) {
+
+        long now = System.currentTimeMillis();
+
+        Graph blueGraph = (Graph) new AbstractObjectGraphVisitor() {
+
+            private Graph blueGraph = new TinkerGraph();
+
+            @Override
+            protected Object visitRoot( org.drools.semantics.Thing root ) {
+                visitNode( root );
+                return blueGraph;
+            }
+
+            @Override
+            protected void visitRelationEdge( org.drools.semantics.Thing node, String prop, Object tgt ) {
+                if ( blueGraph.getVertex( tgt ) == null ) {
+                    blueGraph.addVertex( tgt );
+                }
+                blueGraph.addEdge( UUID.randomUUID(), blueGraph.getVertex( node.getRdfId() ), blueGraph.getVertex( tgt ), prop );
+                if ( tgt instanceof ThingImpl ) {
+                    visitNode( (Thing) tgt );
+                }
+            }
+
+            @Override
+            protected void visitTypeEdge( org.drools.semantics.Thing node, URI tgt ) {
+                visitRelationEdge( node, "isA", tgt );
+            }
+
+            @Override
+            protected void postVisitNode( org.drools.semantics.Thing node ) {
+            }
+
+            @Override
+            protected void preVisitNode( org.drools.semantics.Thing node ) {
+                if ( blueGraph.getVertex( node.getRdfId() ) == null ) {
+                    blueGraph.addVertex( node.getRdfId() );
+                }
+            }
+
+        }.visit( o );
+
+        System.err.println( " DUMPING AS OBJECT GRAPH : time elapsed >> " + ( System.currentTimeMillis() - now ) );
+
+        return blueGraph;
+    }
 
 
     private com.tinkerpop.blueprints.Graph asPropertyGraph(OWLOntology onto) {
 
-        long now = new Date().getTime();
+        long now = System.currentTimeMillis();
         Set<OWLNamedIndividual> inds = onto.getIndividualsInSignature( false );
         final com.tinkerpop.blueprints.Graph blueGraph = new TinkerGraph();
 
@@ -225,7 +313,7 @@ public class ModelManagerOwlAPIHermit {
                 if ( tgt == null ) {
                     tgt = blueGraph.addVertex( object );
                 }
-                blueGraph.addEdge( UUID.randomUUID(), src, tgt, ((URI) pred).getLocalName() );
+                blueGraph.addEdge( UUID.randomUUID(), src, tgt, ((org.openrdf.model.URI) pred).getLocalName() );
 
             }
         };
@@ -236,7 +324,7 @@ public class ModelManagerOwlAPIHermit {
                 ax.accept( tinker );
             }
         }
-        System.err.println( " DUMPING AS GRAPH : time elapsed >> " + ( new Date().getTime() - now ) );
+        System.err.println( " DUMPING AS GRAPH : time elapsed >> " + ( System.currentTimeMillis() - now ) );
 
 
         return blueGraph;
@@ -244,7 +332,7 @@ public class ModelManagerOwlAPIHermit {
     }
 
 
-    private void dumpInSailRDFStore( OWLOntology onto ) throws RepositoryException {
+    private Repository dumpInSailRDFStore( OWLOntology onto ) throws RepositoryException {
 
         long now = new Date().getTime();
         Sail sail = new MemoryStore();
@@ -279,7 +367,7 @@ public class ModelManagerOwlAPIHermit {
             @Override
             protected void addTriple(Object subject, Object pred, Object object) {
                 try {
-                    conn.add( (Resource) subject, (URI) pred, (Value) object );
+                    conn.add( (Resource) subject, (org.openrdf.model.URI) pred, (Value) object );
                 } catch ( RepositoryException re ) {
                     re.printStackTrace();
                 }
@@ -294,9 +382,9 @@ public class ModelManagerOwlAPIHermit {
         }
         conn.commit();
         conn.close();
-        repo.shutDown();
 
         System.err.println( " DUMPING INTO SAIL : time elapsed >> " + ( new Date().getTime() - now ) );
+        return repo;
     }
 
 
--- editor-core/src/test/java/ModelManagerTest.java
+++ editor-core/src/test/java/ModelManagerTest.java
@@ -1,8 +1,26 @@
+import com.clarkparsia.empire.Empire;
+import com.clarkparsia.empire.EmpireOptions;
+import com.clarkparsia.empire.config.ConfigKeys;
+import com.clarkparsia.empire.config.EmpireConfiguration;
+import com.clarkparsia.empire.sesametwo.OpenRdfEmpireModule;
+import com.clarkparsia.empire.sesametwo.RepositoryDataSourceFactory;
+import edu.asu.sharpc2b.prr.ExecutableRuleSet;
+import edu.asu.sharpc2b.prr.ExecutableRuleSetImpl;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.spi.PersistenceProvider;
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.Assert.assertTrue;
 
 public class ModelManagerTest {
 
     @Test
+    @Ignore
     public void testOntologyLoad() throws Exception {
 
         ModelManagerOwlAPIHermit mm = new ModelManagerOwlAPIHermit();
@@ -13,6 +31,7 @@ public class ModelManagerTest {
 
 
     @Test
+    @Ignore
     public void testExampleLoad() throws Exception {
 
         ModelManagerOwlAPIHermit mm = new ModelManagerOwlAPIHermit();
@@ -20,6 +39,75 @@ public class ModelManagerTest {
         mm.processExample();
 
     }
+
+    @Test
+    @Ignore
+    public void testEmpireResourceAccess() {
+//        URL res = ModelManagerTest.class.getResource( "META-INF/empire.configuration.file" );
+//        System.setProperty( "empire.configuration.file", res.getPath() );
+        OpenRdfEmpireModule mod = new OpenRdfEmpireModule();
+        EmpireConfiguration ec = new EmpireConfiguration();
+        Empire.init( ec, mod );
+        EmpireOptions.STRICT_MODE = false;
+
+        PersistenceProvider aProvider = Empire.get().persistenceProvider();
+
+        EntityManagerFactory emf = aProvider.createEntityManagerFactory(
+                "sharp",
+                getTestEMConfigMap() );
+        EntityManager em = emf.createEntityManager();
+
+        assertTrue( em.isOpen() );
+
+
+    }
+
+    @Test
+    @Ignore
+        public void testEmpireResourceRefresh() {
+    //        URL res = ModelManagerTest.class.getResource( "META-INF/empire.configuration.file" );
+    //        System.setProperty( "empire.configuration.file", res.getPath() );
+            OpenRdfEmpireModule mod = new OpenRdfEmpireModule();
+            EmpireConfiguration ec = new EmpireConfiguration();
+            Empire.init( ec, mod );
+            EmpireOptions.STRICT_MODE = false;
+
+            PersistenceProvider aProvider = Empire.get().persistenceProvider();
+
+            EntityManagerFactory emf = aProvider.createEntityManagerFactory(
+                    "sharp",
+                    getTestEMConfigMap() );
+            EntityManager em = emf.createEntityManager();
+
+        ExecutableRuleSet rs = new ExecutableRuleSetImpl();
+            rs.addName( "test" );
+        em.persist( rs );
+
+//        em.close();
+//        emf.close();
+
+
+//        emf = aProvider.createEntityManagerFactory(
+//                "edu.asu.sharpc2b.example1:edu.asu.sharpc2b.lmm:edu.asu.sharpc2b.ocl:edu.asu.sharpc2b.ops:edu.asu.sharpc2b.prr:org.w3._2002._07.owl",
+//                getTestEMConfigMap() );
+//        em = emf.createEntityManager();
+
+        Object x = em.find( ExecutableRuleSetImpl.class, rs.getRdfId() );
+
+        System.out.println( x );
+        System.out.println( ((ExecutableRuleSetImpl) x).getPropertyNames() );
+        }
+
+    private static Map<String, String> getTestEMConfigMap() {
+            Map<String, String> aMap = new HashMap<String, String>();
+
+            aMap.put(ConfigKeys.FACTORY, RepositoryDataSourceFactory.class.getName());
+            aMap.put(RepositoryDataSourceFactory.REPO, "test-repo");
+            aMap.put(RepositoryDataSourceFactory.FILES, "");
+            aMap.put(RepositoryDataSourceFactory.QUERY_LANG, RepositoryDataSourceFactory.LANG_SERQL);
+
+            return aMap;
+        }
 
 
 
