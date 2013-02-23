86c145f17908db7eeff09ff1b35759b17401e235
sotty <dsotty@gmail.com>
2013-02-22 18:37:55 -0700

    Importer : simple type lists

--- model-importer/src/test/java/test/TestSimple.java
+++ model-importer/src/test/java/test/TestSimple.java
@@ -22,8 +22,12 @@ import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDatatype;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.vocab.Namespaces;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
 import org.semanticweb.owlapi.vocab.OWLFacet;
 import org.test.XSD2OWL;
@@ -190,4 +194,233 @@ public class TestSimple {
         }
 
     }
+
+
+
+
+    @Test
+    public void testSimpleList() {
+
+        try {
+            XSD2OWL converter = new XSD2OWL();
+
+            Schema x = converter.parse( "test/simpleList.xsd" );
+            String tns = x.getTargetNamespace() + "#";
+
+            OWLOntology onto = converter.transform( x, true, true );
+
+            OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+            OWLDataFactory factory = manager.getOWLDataFactory();
+
+            OWLDatatype score = factory.getOWLDatatype( IRI.create( tns, "score" ) );
+            OWLDatatype hiscore = factory.getOWLDatatype( IRI.create( tns, "highScore" ) );
+            OWLDatatype maxscore = factory.getOWLDatatype( IRI.create( tns, "dTwentyType" ) );
+
+            OWLClass die = factory.getOWLClass( IRI.create( tns, "Die" ) );
+            OWLClass list = factory.getOWLClass( IRI.create( Namespaces.XSD.toString(), "List" ) );
+            OWLClass d6 = factory.getOWLClass( IRI.create( tns, "dSix" ) );
+            OWLClass d8 = factory.getOWLClass( IRI.create( tns, "dEight" ) );
+            OWLClass d20 = factory.getOWLClass( IRI.create( tns, "dTwenty" ) );
+
+            OWLObjectProperty roll = factory.getOWLObjectProperty( IRI.create( tns, "roll" ) );
+            OWLObjectProperty rock = factory.getOWLObjectProperty( IRI.create( tns, "rock" ) );
+            OWLObjectProperty troll = factory.getOWLObjectProperty( IRI.create( tns, "troll" ) );
+
+            OWLDataProperty item = factory.getOWLDataProperty( IRI.create( Namespaces.XSD.toString(), "item" ) );
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( score ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDatatypeDefinitionAxiom(
+                            score,
+                            factory.getOWLDatatypeRestriction(
+                                    OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ),
+                                    OWLFacet.MIN_INCLUSIVE,
+                                    factory.getOWLLiteral( "1", OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ) ) )
+                    )
+            ));
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDatatypeDefinitionAxiom(
+                            score,
+                            factory.getOWLDatatypeRestriction(
+                                    OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ),
+                                    OWLFacet.MAX_INCLUSIVE,
+                                    factory.getOWLLiteral( "6", OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ) ) )
+                    )
+            ));
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( hiscore ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDatatypeDefinitionAxiom(
+                            hiscore,
+                            factory.getOWLDatatypeRestriction(
+                                    OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ),
+                                    OWLFacet.MIN_INCLUSIVE,
+                                    factory.getOWLLiteral( "1", OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ) ) )
+                    )
+            ));
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDatatypeDefinitionAxiom(
+                            hiscore,
+                            factory.getOWLDatatypeRestriction(
+                                    OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ),
+                                    OWLFacet.MAX_INCLUSIVE,
+                                    factory.getOWLLiteral( "8", OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ) ) )
+                    )
+            ));
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( maxscore ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDatatypeDefinitionAxiom(
+                            maxscore,
+                            factory.getOWLDatatypeRestriction(
+                                    OWL2DatatypeImpl.getDatatype(OWL2Datatype.XSD_INT),
+                                    OWLFacet.MIN_INCLUSIVE,
+                                    factory.getOWLLiteral("1", OWL2DatatypeImpl.getDatatype(OWL2Datatype.XSD_INT)))
+                    )
+            ));
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDatatypeDefinitionAxiom(
+                            maxscore,
+                            factory.getOWLDatatypeRestriction(
+                                    OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ),
+                                    OWLFacet.MAX_INCLUSIVE,
+                                    factory.getOWLLiteral( "20", OWL2DatatypeImpl.getDatatype( OWL2Datatype.XSD_INT ) ) )
+                    )
+            ));
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( list ) ) );
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( item ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDataPropertyDomainAxiom(
+                            item,
+                            list
+                    )
+            ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLDataPropertyRangeAxiom(
+                            item,
+                            factory.getOWLDataUnionOf( maxscore, score, hiscore )
+                    )
+            ) );
+
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( d6 ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLSubClassOfAxiom(
+                            d6,
+                            list
+                    )
+            ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLSubClassOfAxiom(
+                            d6,
+                            factory.getOWLDataAllValuesFrom( item, score )
+                    )
+            ) );
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( d8 ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLSubClassOfAxiom(
+                            d8,
+                            list
+                    )
+            ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLSubClassOfAxiom(
+                            d8,
+                            factory.getOWLDataAllValuesFrom( item, hiscore )
+                    )
+            ) );
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( d20 ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLSubClassOfAxiom(
+                            d20,
+                            list
+                    )
+            ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLSubClassOfAxiom(
+                            d20,
+                            factory.getOWLDataAllValuesFrom( item, maxscore )
+                    )
+            ) );
+
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( roll ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLObjectPropertyDomainAxiom(
+                            roll,
+                            die
+                    )
+            ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLObjectPropertyRangeAxiom(
+                            roll,
+                            d6
+                    )
+            ) );
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( rock ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLObjectPropertyDomainAxiom(
+                            rock,
+                            die
+                    )
+            ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLObjectPropertyRangeAxiom(
+                            rock,
+                            d8
+                    )
+            ) );
+
+            assertTrue( onto.containsAxiom( factory.getOWLDeclarationAxiom( troll ) ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLObjectPropertyDomainAxiom(
+                            troll,
+                            die
+                    )
+            ) );
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLObjectPropertyRangeAxiom(
+                            troll,
+                            d20
+                    )
+            ) );
+
+            assertTrue( onto.containsAxiom(
+                    factory.getOWLSubClassOfAxiom(
+                            die,
+                            factory.getOWLObjectIntersectionOf(
+                                    factory.getOWLObjectIntersectionOf(
+                                            factory.getOWLObjectMinCardinality( 1, rock, d8 ),
+                                            factory.getOWLObjectMaxCardinality( 1, rock, d8 ),
+                                            factory.getOWLObjectAllValuesFrom( rock, d8 )
+                                    ),
+                                    factory.getOWLObjectIntersectionOf(
+                                            factory.getOWLObjectMinCardinality( 1, roll, d6 ),
+                                            factory.getOWLObjectMaxCardinality( 1, roll, d6 ),
+                                            factory.getOWLObjectAllValuesFrom( roll, d6 )
+
+                                    ),
+                                    factory.getOWLObjectIntersectionOf(
+                                            factory.getOWLObjectMinCardinality( 1, troll, d20 ),
+                                            factory.getOWLObjectMaxCardinality( 1, troll, d20 ),
+                                            factory.getOWLObjectAllValuesFrom( troll, d20 )
+                                    )
+                            )
+                    )
+            ) );
+
+
+
+
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+
+    }
+
 }
