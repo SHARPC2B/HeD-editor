5a35d5997e258a84924bb6e5d9f6a084c9d543c4
edinardo <edinardo.potrich@gmail.com>
2013-07-16 10:56:04 -0600

    case 3045: Create Background Information Screen Mockup

--- editor-ui/src/main/webapp/js/app.js
+++ editor-ui/src/main/webapp/js/app.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module('ruleApp', ['ruleApp.controllers'])
+var ruleApp = angular.module('ruleApp', ['ruleApp.controllers', 'ngGrid'])
 	.config([ '$routeProvider', function($routeProvider) {
 			$routeProvider.when('/home', {
 				templateUrl : 'partials/home.html',
 				controller : 'HomeCtrl'
 			});
-			$routeProvider.when('/standard', {
-				templateUrl : 'partials/standard.html',
-				controller : 'StandardCtrl'
+			$routeProvider.when('/standard/background', {
+				templateUrl : 'partials/standard/background.html',
+				controller : 'BackgroundCtrl'
 			});
 			$routeProvider.when('/graph', {
 				templateUrl : 'partials/graph.html',
--- editor-ui/src/main/webapp/js/controllers.js
+++ editor-ui/src/main/webapp/js/controllers.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('ruleApp.controllers', [])
-	.controller('HomeCtrl', [ function() {
-		
+	.controller('HomeCtrl', [ '$http', '$scope', function($http, $scope) {
+		$scope.$parent.title = '&nbsp;';
+		$scope.$parent.menuItems = [
+		                            {"text": "Standard Mode", "href": "#/standard/background"},
+		                            {"text": "Advanced Mode", "href": "#/advanced"},
+		                            {"text": "Revise My Rules", "href": "#/revise"},
+		                            {"text": "Use Others' Rules", "href": "#/others"},
+		                            {"text": "Graphical Rules", "href": "#/graph"},
+		                            {"text": "Help", "href": "#/help"}
+		                           ];
 	} ])
-	.controller('MyCtrl2', [ function() {
+	.controller('BackgroundCtrl', [ '$http', '$scope', function($http, $scope) {
+		$scope.$parent.title = 'Background Information';
+		$scope.$parent.menuItems = [{"text": "Background Information", "href": "#/standard/background", "status": "disabled"},
+		                            {"text": "Select Trigger", "href": "#/standard/trigger"},
+		                            {"text": "Define Logic", "href": "#/standard/logic"},
+		                            {"text": "Choose Action", "href": "#/standard/action"},
+		                            {"text": "Review & Save", "href": "#/standard/save"}];
+		$scope.reference = [{Name: "Guide to clinical Preventive Services 2012", Reference: "http://www.ahrq.gov/professionals/clinicians-providers/guidelines-", Type: "Web"},
+		                    {Name: "Diagnosis of functional kidney failure of cirrhosis with Dopple sonography:", Reference: "X&Y Journal; p. 234-246", Type: "Journal"}];
+	    $scope.gridOptions = { data: 'reference' };
 	} ])
 	.controller('GraphCtrl', [ '$http', '$scope', function ($http, $scope) {
 		$http.get('partials/tree.json').success(function(data) {
